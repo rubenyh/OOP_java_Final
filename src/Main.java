792f221e8b2fe87bd197203fe0b00fa954563aed
rubenyh <rubenyanezhernandez@gmail.com>
2025-05-19 10:06:44 -0700

    server, cliente, juego

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,4 @@
 public class Main {
-    // public static void main(String[] args) throws Exception {
-      
-    //     Juego juego1 = new Juego("Juego1", 9000, "127.0.0.1", 9001);
-    //     Juego juego2 = new Juego("Juego2", 9001, "127.0.0.1", 9000);
-
-    //     juego1.setVisible(true);
-    //     juego2.setVisible(true);
-    // }
     
     public static void main(String[] args) throws Exception {
         new Cliente("127.0.0.1", "127.0.0.1", "Cliente");
