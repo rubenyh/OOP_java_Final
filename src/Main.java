ca046b24bd4b3647e102de88de698a16ab9f3e06
rubenyh <rubenyanezhernandez@gmail.com>
2025-05-21 10:31:04 -0700

    client

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     
     public static void main(String[] args) throws Exception {
-        new Cliente("127.0.0.1", "127.0.0.1", "Cliente");
+        new Cliente("127.0.0.1", 9000, "Cliente");
     }
 }
